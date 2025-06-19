82b71a2b4c969bf7cd1300f1d670d0bb67290073
YD525 <525084464@qq.com>
2025-06-19 10:50:10 +0800

    UPDate

---
+++ PhoenixEngine/SSELexiconBridge/NativeBridge.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixEngine.SSELexiconBridge
+{
+    /// <summary>
+    /// For SSE Lexicon
+    /// </summary>
+    public class NativeBridge
+    {
+    }
+}
