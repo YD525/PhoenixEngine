826855027093c42becb0fb54b6d88dc1644c400b
YD525 <525084464@qq.com>
2025-12-28 07:41:02 +0800

    UPDate

--- PhoenixEngine/LanguageDetector/PersianHelper.cs
+++ PhoenixEngine/LanguageDetector/PersianHelper.cs
@@ -2,11 +2,88 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PhoenixEngine.LanguageDetector
 {
-    public class PersianHelper
+    public static class PersianHelper
     {
+        // Common Persian function words / particles
+        private static readonly string[] PersianKeywords = new[]
+        {
+            "و",       // and
+            "در",      // in / at
+            "به",      // to
+            "از",      // from / of
+            "این",     // this
+            "آن",      // that
+            "برای",    // for
+            "که",      // that
+            "با",      // with
+            "یک",      // a / one
+            "را",      // object marker
+            "است",     // is / be
+            "می",      // present tense marker
+            "شد",      // past tense marker
+            "بر",      // on / over
+            "هم",      // also / too
+            "تا"       // until
+        };
+
+        // Persian Unicode block: U+0600 – U+06FF
+        private static readonly Regex PersianCharRegex = new Regex(
+            "[\u0600-\u06FF]",
+            RegexOptions.Compiled);
+
+        /// <summary>
+        /// Determines whether the input text is likely Persian
+        /// </summary>
+        /// <param name="Input">The text to check</param>
+        /// <param name="KeywordThreshold">Minimum number of keyword matches required (default 1)</param>
+        /// <param name="CharRatioThreshold">Minimum ratio of Persian characters (default 0.3)</param>
+        /// <returns>True if the text is probably Persian, otherwise false</returns>
+        public static bool IsProbablyPersian(
+            string Input,
+            int KeywordThreshold = 1,
+            double CharRatioThreshold = 0.3)
+        {
+            if (string.IsNullOrWhiteSpace(Input))
+                return false;
+
+            int TotalLength = Input.Length;
+            if (TotalLength == 0)
+                return false;
+
+            int PersianCharCount = PersianCharRegex.Matches(Input).Count;
+            double PersianCharRatio = (double)PersianCharCount / TotalLength;
+
+            int KeywordHits = PersianKeywords.Count(k => Input.IndexOf(k, StringComparison.Ordinal) >= 0);
+
+            // Consider Persian if enough characters and at least some keywords
+            return PersianCharRatio >= CharRatioThreshold &&
+                   KeywordHits >= KeywordThreshold;
+        }
+
+        /// <summary>
+        /// Calculates a score indicating the likelihood that the text is Persian
+        /// </summary>
+        /// <param name="Input">The text to score</param>
+        /// <returns>A score representing the likelihood of Persian language</returns>
+        public static double GetPersianScore(string Input)
+        {
+            if (string.IsNullOrWhiteSpace(Input))
+                return 0;
+
+            int TotalLength = Input.Length;
+            if (TotalLength == 0)
+                return 0;
+
+            int PersianCharCount = PersianCharRegex.Matches(Input).Count;
+            int KeywordHits = PersianKeywords.Count(k => Input.IndexOf(k, StringComparison.Ordinal) >= 0);
+
+            // Persian chars are more reliable than keywords
+            return (PersianCharCount * 1.5 + KeywordHits * 2.0) / TotalLength;
+        }
     }
 }
--- PhoenixEngine/LanguageDetector/ThaiHelper.cs
+++ PhoenixEngine/LanguageDetector/ThaiHelper.cs
@@ -2,11 +2,90 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace PhoenixEngine.LanguageDetector
 {
-    public class ThaiHelper
+
+    public static class ThaiHelper
     {
+        private static readonly string[] ThaiKeywords = new[]
+        {
+        "และ",   // and
+        "ของ",   // of
+        "เป็น",   // is / be
+        "มี",     // have
+        "ไม่",    // not
+        "ได้",    // can / got
+        "ว่า",    // that
+        "ใน",     // in
+        "ที่",    // that / which
+        "กับ",    // with
+        "ก็",     // also
+        "จะ",     // will
+        "มา",     // come
+        "ไป",     // go
+        "คน",     // person
+        "อะไร",   // what
+        "นี่",    // this
+        "นั้น"    // that
+    };
+
+        private static readonly Regex ThaiCharRegex = new Regex(
+            "[\u0E00-\u0E7F]",
+            RegexOptions.Compiled);
+
+        /// <summary>
+        /// Determines whether the input text is likely Thai
+        /// </summary>
+        /// <param name="Input">The text to check</param>
+        /// <param name="KeywordThreshold">Minimum number of keyword matches required (default 1)</param>
+        /// <param name="CharRatioThreshold">Minimum ratio of Thai characters (default 0.3)</param>
+        /// <returns>True if the text is probably Thai, otherwise false</returns>
+        public static bool IsProbablyThai(
+            string Input,
+            int KeywordThreshold = 1,
+            double CharRatioThreshold = 0.3)
+        {
+            if (string.IsNullOrWhiteSpace(Input))
+                return false;
+
+            int TotalLength = Input.Length;
+            if (TotalLength == 0)
+                return false;
+
+            int ThaiCharCount = ThaiCharRegex.Matches(Input).Count;
+            double ThaiCharRatio = (double)ThaiCharCount / TotalLength;
+
+            int KeywordHits = ThaiKeywords.Count(k => Input.IndexOf(k, StringComparison.Ordinal) >= 0);
+
+            // Core rule:
+            // - Enough Thai characters
+            // - And at least some Thai keywords
+            return ThaiCharRatio >= CharRatioThreshold &&
+                   KeywordHits >= KeywordThreshold;
+        }
+
+        /// <summary>
+        /// Calculates a score indicating the likelihood that the text is Thai
+        /// </summary>
+        /// <param name="Input">The text to score</param>
+        /// <returns>A score representing the likelihood of Thai language</returns>
+        public static double GetThaiScore(string Input)
+        {
+            if (string.IsNullOrWhiteSpace(Input))
+                return 0;
+
+            int TotalLength = Input.Length;
+            if (TotalLength == 0)
+                return 0;
+
+            int ThaiCharCount = ThaiCharRegex.Matches(Input).Count;
+            int KeywordHits = ThaiKeywords.Count(k => Input.IndexOf(k, StringComparison.Ordinal) >= 0);
+
+            // Thai chars are much more reliable than keywords
+            return (ThaiCharCount * 1.5 + KeywordHits * 2.0) / TotalLength;
+        }
     }
 }
