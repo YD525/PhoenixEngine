8ee1696172ad37d123df3a5bffa99af43842c96e
YD525 <525084464@qq.com>
2025-12-28 18:26:30 +0800

    UPDate

--- PhoenixEngine/EngineManagement/Engine.cs
+++ PhoenixEngine/EngineManagement/Engine.cs
@@ -14,7 +14,7 @@ namespace PhoenixEngine.EngineManagement
 {
     public class Engine
     {
-        public static string Version = "1.1.8.8";
+        public static string Version = "1.1.8.9";
         public static string CurrentPath = "";
         /// <summary>
         /// Instance of the local SQLite database helper.
