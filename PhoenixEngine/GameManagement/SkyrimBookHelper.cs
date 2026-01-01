8372e8ad745f3f637a533c2eeec98f4a0609b3a2
YD525 <525084464@qq.com>
2026-01-01 15:45:44 +0800

    UPDate

--- PhoenixEngine/GameManagement/SkyrimBookHelper.cs
+++ PhoenixEngine/GameManagement/SkyrimBookHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using PhoenixEngine.TranslateManage;
+using static PhoenixEngine.GameManagement.SkyrimBookHelper;
 using static PhoenixEngine.TranslateManagement.ChunkHelper;
 
 namespace PhoenixEngine.GameManagement
@@ -9,12 +10,12 @@ namespace PhoenixEngine.GameManagement
     {
         public class CheckChar
         {
-            public string Char = "";
-            public bool IsStartOrEnd = false;
-            public CheckChar(string Char, bool IsStartOrEnd)
+            public string StartChar = "";
+            public string EndChar = "";
+            public CheckChar(string Start, string End)
             { 
-                this.Char = Char;
-                this.IsStartOrEnd = IsStartOrEnd;
+                this.StartChar = Start;
+                this.EndChar = End;
             }
         }
 
@@ -23,16 +24,28 @@ namespace PhoenixEngine.GameManagement
         public bool IsInit = false;
         public void Init()
         {
+            CheckChars.Add(new CheckChar("<",">"));
+            CheckChars.Add(new CheckChar("[", "]"));
+
             IsInit = true;
         }
         public List<UnitChunk> ChunkBook(TranslationUnit Unit)
         {
+            //Okay, I just need to take care of this.
+            //My real concern is that if the user isn't using local AI, but rather cloud-based AI, SSELex, due to its context-aware generation, might waste a lot of tokens.
+
             if (!IsInit)
             {
                 Init();
             }
-            //Okay, I just need to take care of this.
-            //My real concern is that if the user isn't using local AI, but rather cloud-based AI, SSELex, due to its context-aware generation, might waste a lot of tokens.
+
+            string GetBookContent = Unit.SourceText;
+
+            for (int i = 0; i < GetBookContent.Length; i++)
+            {
+                string GetChar = GetBookContent.Substring(i,1);
+            }
+           
             return new List<UnitChunk>();
         }
     }
