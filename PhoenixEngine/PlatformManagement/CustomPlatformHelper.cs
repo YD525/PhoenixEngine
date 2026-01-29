cc2bac3e132260f94bbf10ad1ee969ad25363790
YD525 <525084464@qq.com>
2026-01-29 10:58:03 +0800

    UPDate

--- PhoenixEngine/PlatformManagement/CustomPlatformHelper.cs
+++ PhoenixEngine/PlatformManagement/CustomPlatformHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace PhoenixEngine.PlatformManagement
 {
@@ -13,25 +14,93 @@ namespace PhoenixEngine.PlatformManagement
 
     public class CustomCore
     {
-        public string Url = "";
-        public string PayLoad { get; set; }
+        private string _Url = "";
+        private string _Header = "";
+        private string _PayLoad = "";
         public bool IsPost { get; set; }
 
-        public QueryRuleItem QueryRule = new QueryRuleItem();
-        public string MakeUrl(string Url, List<ReplaceTag> Tags)
-        { 
-            
-        }
-        public WebHeaderCollection MakeHeader(string HeaderStr, List<ReplaceTag> Tags)
-        { 
-        
-        }
-        public string MakePayLoad(string PayLoad, List<ReplaceTag> Tags)
-        { 
-        
+        public void SetUrl(string Url)
+        {
+            this._Url = HttpUtility.UrlDecode(Url);
         }
 
-        public string QueryReturn(string ReturnStr, QueryRuleItem QueryRule)
+        public QueryRuleItem QueryRule = new QueryRuleItem();
+        public string GenUrl(List<ReplaceTag> Tags)
+        {
+            string NewUrl = string.Copy(_Url);
+
+            foreach (var GetTag in Tags)
+            {
+                NewUrl.Replace(GetTag.Tag,GetTag.Value);
+            }
+
+            return NewUrl;
+        }
+        public List<CustomKeyValue> GetUrlKeyValues()
+        {
+            List<CustomKeyValue> CustomKeyValues = new List<CustomKeyValue>();
+            if (_Url.Contains("?"))
+            {
+                string GetRightStr = _Url.Substring(_Url.IndexOf("?") + "?".Length);
+                foreach (var GetParam in GetRightStr.Split('&'))
+                {
+                    if (GetParam.Trim().Length > 0 && GetParam.Contains("="))
+                    {
+                        string GetKey = GetParam.Split('=')[0];
+                        string GetValue = GetParam.Split('=')[1];
+                        CustomKeyValues.Add(new CustomKeyValue(GetKey,GetValue));
+                    }
+                }
+            }
+            return CustomKeyValues;
+        }
+        public void SetHeader(string Header)
+        { 
+            this._Header = Header;
+        }
+        public WebHeaderCollection GenHeader(List<ReplaceTag> Tags)
+        {
+            WebHeaderCollection Header = new WebHeaderCollection();
+            foreach (var GetLine in _Header.Split(new char[2] { '\r', '\n' }))
+            {
+                if (GetLine.Trim().Length > 0 && GetLine.Contains(":"))
+                {
+                    string GetKey = GetLine.Split(':')[0];
+
+                    string GetValue = GetLine.Split(':')[1];
+
+                    foreach (var GetTag in Tags)
+                    {
+                        if (GetTag.Key.Equals(GetKey))
+                        {
+                            GetValue = GetTag.Value;
+                            break;
+                        }
+                    }
+                    
+                    Header.Add(GetKey, GetValue);
+                }
+            }
+            return Header;
+        }
+        public List<CustomKeyValue> GetHeaderKeyValues()
+        {
+            List<CustomKeyValue> CustomKeyValues = new List<CustomKeyValue>();
+
+            foreach (var GetLine in _Header.Split(new char[2] { '\r', '\n' }))
+            {
+                if (GetLine.Trim().Length > 0 && GetLine.Contains(":"))
+                {
+                    string GetKey = GetLine.Split(':')[0];
+                    string GetValue = GetLine.Split(':')[1];
+                    CustomKeyValues.Add(new CustomKeyValue(GetKey, GetValue));
+                }
+            }
+
+            return CustomKeyValues;
+        }
+       
+        public string MakePayLoad(string PayLoad, List<ReplaceTag> Tags)
         { 
         
         }
@@ -41,16 +110,36 @@ namespace PhoenixEngine.PlatformManagement
     {
         public string FieldName { get; set; }
         public bool ByJson = true;
-        public bool IgnoreCase = true;
 
         public string LeftStr = "";
         public string RightStr = "";
+        public string SplitStr = "";
+    }
+
+    public class CustomKeyValue
+    {
+        public string Key = "";
+        public string Value = "";
+
+        public CustomKeyValue(string Key, string Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+        }
     }
 
     public class ReplaceTag
     {
         public string Tag = "";
+        public string Key = "";
         public string Value = "";
+
+        public ReplaceTag(string Key, string Value)
+        {
+            this.Key = Key;
+            this.Value = Value;
+            this.Tag = "{" + Key + "}";
+        }
     }
 
     public enum CustomPlatformType
