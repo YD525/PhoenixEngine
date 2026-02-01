bae8e07e7b98302fdcc8f7f87ca2c467f4de1031
YD525 <525084464@qq.com>
2026-02-02 07:24:06 +0800

    UPDate

--- PhoenixEngine/Properties/AssemblyInfo.cs
+++ PhoenixEngine/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PhoenixEngine")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright (C) 2026 YD525")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
