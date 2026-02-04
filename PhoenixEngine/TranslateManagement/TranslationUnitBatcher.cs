657421dcf5cb916adfc15ad4acbcaa9dec672dfa
YD525 <525084464@qq.com>
2026-02-05 02:09:06 +0800

    UPDate

---
+++ PhoenixEngine/TranslateManagement/TranslationUnitBatcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PhoenixEngine.TranslateCore;
+using PhoenixEngine.TranslateManage;
+
+namespace PhoenixEngine.TranslateManagement
+{
+    public class TranslationUnitBatcher
+    {
+        public static int TextLengthLimit = 1000;
+        public class BatchTranslationUnit
+        {
+            public Languages From;
+            public Languages To;
+
+            public int TextLength = 0;
+            public string[] ContextTokens;
+
+            public List<TranslationUnit> TranslationUnits = new List<TranslationUnit>();
+        }
+
+        public static List<BatchTranslationUnit> MergeUnits(List<TranslationUnit> Units)
+        {
+            return new List<BatchTranslationUnit>();
+        }
+    }
+   
+}
