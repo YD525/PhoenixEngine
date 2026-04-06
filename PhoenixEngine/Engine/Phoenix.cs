6c31af02195325627dae2bdc04976b5de31db087
YD525 <525084464@qq.com>
2026-04-06 23:41:59 +0800

    UPDate

--- PhoenixEngine/Engine/Phoenix.cs
+++ PhoenixEngine/Engine/Phoenix.cs
@@ -16,7 +16,7 @@ namespace PhoenixEngine
     public class Phoenix : ConfigExtend
     {
         public static AITranslationMemory AIMemory = new AITranslationMemory();
-        public static string Version = "3.1.5.2";
+        public static string Version = "3.1.5.3";
 
         public static object QueryPlatformDataLock = new object();
 
