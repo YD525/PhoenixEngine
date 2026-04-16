c0070e95a97bb9796e743b280da8a61dc0b2b003
YD525 <525084464@qq.com>
2026-04-17 05:40:28 +0800

    UPDate

---
+++ PhoenixEngine/Language/CzechHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoenixEngine.Language
+{
+    internal static class CzechHelper
+    {
+        //It's not a big problem if this is implemented later; it will only affect some functions of Auto.(AutoTab,LanguageDetect)
+        //Let's save it for later.
+    }
+}
